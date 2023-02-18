7a9dbe331eff84e3259872362026a7f15ba7e43d
Yossi52 <124557821+Yossi52@users.noreply.github.com>
2023-02-18 14:54:26 +0900

    역대 최고 점수를 기록해 주는 기능 추가

--- snake_game/main.py
+++ snake_game/main.py
@@ -7,7 +7,7 @@ from scoreboard import ScoreBoard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Yong's Snack Game")
+screen.title("Yong's Snake Game")
 screen.tracer(0)
 
 snake = Snake()
@@ -38,14 +38,14 @@ def game():
 
         # Detect collision with wall.
         if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
         # Detect collision with tail.
         for segment in snake.segments[1:]:
             if snake.head.distance(segment) < 10:
-                game_is_on = False
-                scoreboard.game_over()
+                scoreboard.reset()
+                snake.reset()
 
 screen.onkey(game, key=" ")
 
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -9,6 +9,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -17,14 +19,16 @@ class ScoreBoard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score : {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.color('red')
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
